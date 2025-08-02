5947c14ea53e08121f0559b5c58f35330752c502
klavrenuk <klavrenuk1993@gmail.com>
2025-08-02 12:29:13 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    Hello from Nicka
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Импорт стилей Bootstrap 5
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Импорт JS Bootstrap 5 (включая Popper.js)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).mount('#app')
