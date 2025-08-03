06b5982dcbdd31de2e57e4dd817c59c35b19c5be
klavrenuk <klavrenuk1993@gmail.com>
2025-08-03 20:30:35 +0300

    Founder

--- src/components/founder/Founder.vue
+++ src/components/founder/Founder.vue
@@ -5,12 +5,21 @@ import FounderInfo from "@/components/founder/FounderInfo.vue";
 <template>
   <section class="founder app-section">
     <div class="app-component-layout">
-      <div></div>
+      <div>
+        <img src="/images/founder-photo.svg" alt="Камбулат Германбиевич" class="founder__photo" />
+      </div>
       <FounderInfo />
     </div>
   </section>
 </template>
 
 <style scoped lang="scss">
+.founder {
+  background: #7A4FF1;
 
+  &__photo {
+    width: 568px;
+    height: 576px;
+  }
+}
 </style>
